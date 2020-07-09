58f63d54b2667ecc12a6dac17fbe3c5c93bf30fb
rfortunski <fortunski.rafal@gmail.com>
2020-07-09 13:08:51 +0200

    Updates

---
+++ precache-manifest.0853e18d4e21dd0efcd0ca4ba74c0204.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "5fe1f54f2bc2c50baaf9567ea33aec50",
+    "url": "./index.html"
+  },
+  {
+    "revision": "af2910b67cc886eb691a",
+    "url": "./static/css/main.e9eb4661.chunk.css"
+  },
+  {
+    "revision": "d092f8ff10369fa5f465",
+    "url": "./static/js/2.edc346ac.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "./static/js/2.edc346ac.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "af2910b67cc886eb691a",
+    "url": "./static/js/main.e73862ed.chunk.js"
+  },
+  {
+    "revision": "4934f41d509ad97a854f",
+    "url": "./static/js/runtime-main.f2e65d07.js"
+  },
+  {
+    "revision": "0dc7e4bea3b89a5a0d9bc50ac241f801",
+    "url": "./static/media/menu-white.0dc7e4be.svg"
+  },
+  {
+    "revision": "24625810c1a06218856ecdf4ace2b75e",
+    "url": "./static/media/menu.24625810.svg"
+  }
+]);
