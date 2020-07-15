8de243e258f4cc702e5eb0dd4da383ef59835941
rfortunski <fortunski.rafal@gmail.com>
2020-07-15 23:38:10 +0200

    Remove duplicated directory

--- src/components/BackgroundImage/BackgroundImage.js
+++
@@ -1,7 +0,0 @@
-import React from "react";
-
-const BackgroundImage = ({ className, style, ...props }) => {
-  return <div className={className} style={style}></div>;
-};
-
-export default BackgroundImage;
--- src/components/Button/Button.js
+++
@@ -1,8 +0,0 @@
-import React from "react";
-import "./Button.scss";
-
-const Button = ({ className, children, ...props }) => {
-  return <button className={className}>{children}</button>;
-};
-
-export default Button;
